40a5c28742ac1c828736bdcb05d250d0dd38a9e8
Vishnu <vishnur1225@gmail.com>
2026-08-23 20:39:09 +0530

    Set up Vendra frontend structure and global layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-
-import './App.css'
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 
 function App() {
-  
-
   return (
     <>
-      
+      <div className="app-container">
+        <Header />
+
+        <main>
+          <div className="content-container">{/* page content */}</div>
+        </main>
+
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
