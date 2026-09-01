5bcc76d0196c303bc55001bd2ee77b0c09e3e2c6
Vishnu <vishnur1225@gmail.com>
2026-09-01 22:27:08 +0530

    create the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,6 +9,18 @@ export function Footer(){
                 <div className="footer-info">About Us</div>
                 <div className="footer-info">Career</div>
             </div>
+            <div className="footer-info-section">
+                <h3>About</h3>
+                <div className="footer-info">Contact Us</div>
+                <div className="footer-info">About Us</div>
+                <div className="footer-info">Career</div>
+            </div>
+            <div className="footer-info-section">
+                <h3>About</h3>
+                <div className="footer-info">Contact Us</div>
+                <div className="footer-info">About Us</div>
+                <div className="footer-info">Career</div>
+            </div>
            
         </div>
     )
