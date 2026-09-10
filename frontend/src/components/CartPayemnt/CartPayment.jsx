9daa26c325653211567d0f99147378fdd3ec3d3b
Vishnu-raghav <vishnur1225@gmail.com>
2026-09-10 22:34:34 +0530

    creating a cart payemnt section just build header and one of the column

--- frontend/src/components/CartPayemnt/CartPayment.jsx
+++ frontend/src/components/CartPayemnt/CartPayment.jsx
@@ -1,10 +1,34 @@
 import "./CartPayment.css"
+import { 
+ MoveLeft,
+ LockKeyhole,
+ ThumbsUp
+ } from "lucide-react";
 
 export function CartPayment(){
 
     return(
-        <div>
-            payemnt section
+        <div className="payemnt-container">
+            <div className="payment-header">
+                <div className="go-back">
+                    <MoveLeft />
+                    <h4>Complete payment</h4>
+                </div>
+
+                <div className="payment-secure">
+                    <LockKeyhole className="payment-secure-icon" />
+                    100% secure
+                </div>
+            </div>
+            <div className="payment-box">
+                <div className="payment-box-entity">
+                    <ThumbsUp />
+                    Recommended for you
+                </div>
+            </div>
+            <div className="payment-box"></div>
+            <div className="payment-box"></div>
+            
         </div>
     )
 }
